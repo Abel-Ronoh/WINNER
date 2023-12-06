af012e20f1a931e36c655444c24b21ffdb128d6f
abel-ronoh <abellronoh@gmail.com>
2023-12-06 13:50:23 +0300

    added 3 files

---
+++ index.js
@@ -0,0 +1,10 @@
+// var box = document.getElementById("box")
+
+// box.addEventListener("click", ()=>{
+//     box.style.bgColor = "red"
+// })
+function colorChange(){
+    var box = document.getElementById("box")
+     box.style.backgroundColor = 'red'
+    alert("you won nothing")
+}
